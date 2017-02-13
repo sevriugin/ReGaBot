9fd10e750e42b80d84ecb5ef20954c9f1ecc01aa
sevriugin <sevriugin@gmail.com>
2017-02-13 18:22:19 +0300

    add express

--- yandex/index.js
+++ yandex/index.js
@@ -1,9 +1,7 @@
-/*-----------------------------------------------------------------------------
-This template demonstrates how to use Waterfalls to collect input from a user using a sequence of steps.
-For a complete walkthrough of creating this type of bot see the article at
-https://docs.botframework.com/en-us/node/builder/chat/dialogs/#waterfall
------------------------------------------------------------------------------*/
 "use strict";
+
+var express = require('express');
+var app = express();
 
 function testHook(req, res) {
     res.status(200);
