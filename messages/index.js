d59ec237a9f7acb5a9a4bc70b8b6d046b408fb0d
sevriugin <sevriugin@gmail.com>
2017-02-13 23:30:18 +0300

    fix 7

--- messages/index.js
+++ messages/index.js
@@ -177,12 +177,14 @@ bot.dialog('/', [
                                         if (!error) {
                                             // Process the message in less than 30 seconds, the message
                                             // text is available in serverMessages[0].messageText
-                                            session.send(serverMessages[0].messageText);
-                                            queueService.deleteMessage(queueName, serverMessages[0].messageId, serverMessages[0].popReceipt, function(error) {
-                                                if (!error) {
-                                                    // Message deleted
-                                                }
-                                            });
+                                            if(serverMessages && Array.isArray(serverMessages)) { 
+                                                session.send(serverMessages[0].messageText);
+                                                queueService.deleteMessage(queueName, serverMessages[0].messageId, serverMessages[0].popReceipt, function(error) {
+                                                    if (!error) {
+                                                        // Message deleted
+                                                    }
+                                                });
+                                            }
                                         }
                                     });
                                 }
