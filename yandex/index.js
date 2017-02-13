0f35f585647125546ef161b9563a816e87d7445e
sevriugin <sevriugin@gmail.com>
2017-02-13 19:42:53 +0300

    add code parameter

--- yandex/index.js
+++ yandex/index.js
@@ -3,10 +3,10 @@
 module.exports = function(context, req) {
     context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-    if (req.query.name || (req.body && req.body.name)) {
+    if (req.query.code || (req.body && req.body.code)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.code || req.body.code)
         };
     }
     else {
