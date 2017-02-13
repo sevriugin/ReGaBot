5ceb3ca9493fc62742112c801df59df088a77584
sevriugin <sevriugin@gmail.com>
2017-02-13 18:38:14 +0300

    add context

--- yandex/index.js
+++ yandex/index.js
@@ -1,7 +1,13 @@
 "use strict";
 
-function testHook(req, res) {
-    console.log('testHook');
+function testHook(context, req) {
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Hello " + (req.query.name || req.body.name)
+    };
+    
+    context.done();
 }
 
 module.exports = { default: testHook }
