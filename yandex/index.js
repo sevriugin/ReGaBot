36c79be1404723e13bd68bb563a48b424da1ea7b
sevriugin <sevriugin@gmail.com>
2017-02-13 18:51:37 +0300

    change handler

--- yandex/index.js
+++ yandex/index.js
@@ -1,13 +1,19 @@
 "use strict";
 
-function testHook(context, req) {
-    
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "Hello " + (req.query.name || req.body.name)
-    };
-    
-    context.done();
-}
+module.exports = function(context, req) {
+    context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
 
-module.exports = { default: testHook }
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done();
+};
